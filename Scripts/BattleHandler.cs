46bf04662cd146d0bb7aaa413725c174a204be19
Dion <38928763+Birdy25@users.noreply.github.com>
2019-02-27 21:58:06 +1000

    Add files via upload
    Added scripts that I edited as part of the assessment.

---
+++ Scripts/BattleHandler.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Static class with method (function) to determine the outcome of a dance battle based on the player and NPC that are 
+///     dancing off against each other.
+///     
+/// TODO:
+///     Battle needs to use stats and random to determine the winner of the dance off
+///       - outcome value to be a float value between 1 and negative 1. 1 being the biggest possible player win over NPC, 
+///         through to -1 being the most decimating defeat of the player possible.
+/// </summary>
+public static class BattleHandler
+{
+    public static void Battle(BattleEventData data)
+    {
+        //This needs to be replaced with some actual battle logic, at present 
+        // we just award the maximum possible win to the player
+        float outcome = 1;        
+        var results = new BattleResultEventData(data.player, data.npc, outcome);
+
+        //Luck is randomised every battle
+        int roundsLuckStat = (Random.Range(1, 101));
+        Debug.Log(roundsLuckStat);
+
+        //checks level
+
+        //battlescore assigned and compared
+        int npcBattleScore = data.npc.rhythm + data.npc.style;
+        int pcBattleScore = data.player.rhythm + data.player.style;
+        Debug.Log("pc" + pcBattleScore);
+        Debug.Log("npc" + npcBattleScore);
+        //check for luck
+        if (data.npc.luck >= roundsLuckStat)
+        {
+            npcBattleScore = npcBattleScore * data.npc.luck;
+        }
+        if (data.player.luck >= roundsLuckStat)
+        {
+            pcBattleScore = pcBattleScore * data.player.luck;
+        }
+
+        //check for win
+        if (pcBattleScore > npcBattleScore)
+        {
+            outcome = 1;
+        }
+        if (npcBattleScore > pcBattleScore)
+        {
+            outcome = 0;
+        }     
+        GameEvents.FinishedBattle(results);
+    }
+}
---
+++ Scripts/StatsGenerator.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+///  A <see langword="static"/> class with methods (functions) for initialising or randomising the stats class.
+///  
+/// TODO:
+///     Initialise a stats instance with generated starting stats
+///     Handle the assignment of extra points or xp into an existing stats of a character
+///         - this is expected to be used by NPCs leveling up to match the player.
+/// </summary>
+public static class StatsGenerator
+{
+    public static void InitialStats(Stats stats)
+    {
+        stats.level = 1;
+        try
+        {
+            stats.level = System.Convert.ToInt32(System.IO.File.ReadAllText("level.txt"));
+        }
+        catch
+        {
+
+        }
+
+        //generates stats on game start
+        stats.rhythm = (Random.Range(5, 10));
+        stats.style = (Random.Range(5, 10));
+        stats.luck = (Random.Range(1, 5));
+        /*data.player.rhythm = (Random.Range(5, 10) * stats.level);
+        data.player.style = (Random.Range(5, 10) * stats.level);
+        data.player.luck = (Random.Range(1, 5) * stats.level);*/
+    }
+
+    public static void AssignUnusedPoints(Stats stats, int points)
+    {
+        double rhythmPoints = points / 2.5;
+        double luckPoints = points / 5;
+        double stylePoints = points / 2.5;
+        Debug.Log(stats.rhythm);
+        Debug.Log(stats.style);
+        Debug.Log(stats.luck);
+        stats.rhythm += System.Convert.ToInt32(rhythmPoints);
+        stats.style += System.Convert.ToInt32(stylePoints);
+        stats.luck += System.Convert.ToInt32(luckPoints);
+        Debug.Log(stats.rhythm);
+        Debug.Log(stats.style);
+        Debug.Log(stats.luck);
+    }
+}
